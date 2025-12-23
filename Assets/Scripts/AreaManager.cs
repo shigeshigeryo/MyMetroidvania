1b337f1769aa48c0cf15a2006834653864ce02bb
shigeta_ryotaro <fantianliangtailang149@gmail.com>
2025-12-23 15:16:36 +0900

    エリアのデータを分割で管理できるようにする

--- Assets/Scripts/AreaManager.cs
+++ Assets/Scripts/AreaManager.cs
@@ -4,8 +4,13 @@ public class AreaManager : MonoBehaviour
 {
     void Start()
     {
-        var test = JsonHandler.LoadResourcesJsonFile<WorldStateData>("WorldStateData");
+        var test = JsonHandler.LoadResourcesJsonFile<WorldStateData>("WorldData/WorldStateData");
         test.TryGetAllAreaTargetState("Ability", out var testTarget);
         Debug.Log(testTarget.TargetId);
+
+        test.TryGetAreaDataPath("Area_001", out var path);
+        var areaTest = JsonHandler.LoadResourcesJsonFile<AreaStateData>(path);
+        areaTest.TryGetTargetState("Door_001", out var doorData);
+        Debug.Log(doorData.TargetId);
     }
 }
--- Assets/Scripts/Data/WorldStateData.cs
+++ Assets/Scripts/Data/WorldStateData.cs
@@ -7,11 +7,10 @@ public class WorldStateData
     private Dictionary<string, TargetStateData> _allAreaTargetStateDataList = new Dictionary<string, TargetStateData>();
     public Dictionary<string, TargetStateData> AllAreaTargetStateDataList => _allAreaTargetStateDataList;
 
-
     // エリアの状態の管理
     // key：対象エリアのID 
-    private Dictionary<string, AreaStateData> _areaStateDataList = new Dictionary<string, AreaStateData>();
-    public Dictionary<string, AreaStateData> AreaStateDataList => _areaStateDataList;
+    private Dictionary<string, string> _areaStateDataPathList = new Dictionary<string, string>();
+    public Dictionary<string, string> AreaStateDataPathList => _areaStateDataPathList;
 
     /// <summary>
     /// IDで検索して存在の可否とエリアの状態データを返却する（全てのエリア共通の対象）
@@ -25,13 +24,13 @@ public class WorldStateData
     }
 
     /// <summary>
-    /// エリアIDで検索して存在の可否とエリアの状態データを返却する
+    /// エリアIDで検索して存在の可否とエリアの状態データを保存しているパスを返却する
     /// </summary>
     /// <param name="areaId"></param>
-    /// <param name="areaStateData">ObjectStateDataを返却する</param>
+    /// <param name="path">ObjectStateDataを返却する</param>
     /// <returns></returns>
-    public bool TryGetAreaState(string areaId, out AreaStateData areaStateData)
+    public bool TryGetAreaDataPath(string areaId, out string path)
     {
-        return AreaStateDataList.TryGetValue(areaId, out areaStateData);
+        return AreaStateDataPathList.TryGetValue(areaId, out path);
     }
 }
