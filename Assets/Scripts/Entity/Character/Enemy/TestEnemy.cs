04a8ab1467579c10ff9df49671ed1429284374a2
shigeta_ryotaro <fantianliangtailang149@gmail.com>
2025-12-29 18:22:47 +0900

    敵の攻撃（仮）を追加

--- Assets/Scripts/Entity/Character/Enemy/TestEnemy.cs
+++ Assets/Scripts/Entity/Character/Enemy/TestEnemy.cs
@@ -1,11 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class TestEnemy : EnemyBase
 {
+    [Header("UŒ‚iTestEnemyj")]
+    [SerializeField, Tooltip("UŒ‚”»’è‚ÌŒ´“_")] private Transform _hitBoxOriginTransform = null;
+    [SerializeField, Tooltip("UŒ‚”»’è")] private HitBox _hitBox;
+    [SerializeField, Tooltip("UŒ‚CTi•bj")] private float _coolSec = 1f;
+    private bool _isAttacking = false;
+
     protected void Start()
     {
         Initialize();
         InitializeEvents();
+
+        StartCoroutine(Attack());
+    }
+
+    private IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(1f);
+        while (true)
+        {
+            _isAttacking = true;
+            _hitBox.SetEnableCollider();
+        
+            yield return new WaitForSeconds(1f);
+
+            _hitBox.SetDisableCollider();
+            _isAttacking = false;
+            yield return new WaitForSeconds(_coolSec);
+        }
     }
 
     protected override void Damaged()
