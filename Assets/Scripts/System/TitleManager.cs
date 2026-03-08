3425fd0218d7c278e64435c48b2fb30f288d4714
shigeta_ryotaro <fantianliangtailang149@gmail.com>
2026-03-08 14:54:26 +0900

    タイトルシーンの処理を追加

---
+++ Assets/Scripts/System/TitleManager.cs
@@ -0,0 +1,64 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MyMetroidVania.System
+{
+    public class TitleManager : MonoBehaviour
+    {
+        public static TitleManager Instance { get; private set; } = null;
+
+        [SerializeField] private Button _startButton = null;
+        [SerializeField] private Button _exitButton = null;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+        private void Start()
+        {
+            AudioManager.Instance.GetAndPlayBgm("BGM_Title");
+
+            if(_startButton != null)
+            {
+                _startButton.onClick.AddListener(LoadInGameScene);
+                _startButton.Select();
+            }
+
+            if (_exitButton != null)
+            {
+                _exitButton.onClick.AddListener(QuitGame);
+            }
+        }
+
+        private void LoadInGameScene()
+        {
+            SceneManager.LoadScene("InGameScene");
+        }
+
+        private void QuitGame()
+        {
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#else
+            Application.Quit();
+#endif
+        }
+
+        private void OnDestroy()
+        {
+            _startButton.onClick.RemoveAllListeners();
+            _exitButton.onClick.RemoveAllListeners();
+        }
+    }
+}
