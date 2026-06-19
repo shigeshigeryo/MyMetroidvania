78cdddec66d1e376c855de6e24d2f5e3c5eb3af0
shigeta_ryotaro <fantianliangtailang149@gmail.com>
2026-06-19 15:48:09 +0900

    refactor マップ関係のスクリプトにコメントの追加と不要なusingの削除

--- Assets/Scripts/System/Map/AreaMap.cs
+++ Assets/Scripts/System/Map/AreaMap.cs
@@ -3,19 +3,28 @@ using UnityEngine;
 
 namespace MyMetroidVania.System.Map
 {
+    /// <summary>
+    /// エリアのマップを管理
+    /// </summary>
     public class AreaMap : MonoBehaviour
     {
         public static Dictionary<string, AreaMap> AreaMapList = new();
 
         [SerializeField, Tooltip("エリアID")] private string _areaId;
         [SerializeField, Tooltip("エリア間のつながりを表現する線の配列")] private LineMap[] _lineMaps = null;
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
         private void Awake()
         {
             AreaMapList.Add(_areaId, this);
             gameObject.SetActive(false);
         }
 
-        // 到達済み状態にする（踏破状態も作る?）
+        /// <summary>
+        /// 到達済み状態にする
+        /// </summary>
         public void SetVisitedState()
         {
             gameObject.SetActive(true);
@@ -26,6 +35,9 @@ namespace MyMetroidVania.System.Map
             }
         }
 
+        /// <summary>
+        /// clean処理
+        /// </summary>
         private void OnDestroy()
         {
             AreaMapList.Remove(_areaId);
--- Assets/Scripts/System/Map/LineMap.cs
+++ Assets/Scripts/System/Map/LineMap.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 namespace MyMetroidVania.System.Map
 {
+    /// <summary>
+    /// エリアとエリアをつなぐ線の管理
+    /// </summary>
     public class LineMap : MonoBehaviour
     {
+        /// <summary>
+        /// 初期化処理
+        /// 最初は非表示
+        /// </summary>
         private void Awake()
         {
             gameObject.SetActive(false);
         }
 
+        /// <summary>
+        /// 表示する
+        /// </summary>
         public void SetActive()
         {
             if (!gameObject.activeSelf) gameObject.SetActive(true);
